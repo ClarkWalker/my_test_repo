7f7916724140c39aede5ea65379dbec5d4e8ad77
Clark Walker <303walker@gmail.com>
2020-07-08 21:16:20 -0600

    initial

---
+++ lib.py
@@ -0,0 +1,5 @@
+
+
+def circle_area(diam):
+    from math import pi as PI
+    return (PI * (diam/2))**2
---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+# entry point
+def main():
+    import lib
+    print( lib.circle_area(10) )
+    # here
+
+
+if __name__ == "__main__":
+    main()
